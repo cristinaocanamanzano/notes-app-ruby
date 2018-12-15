98a683d0ad79032613ed720835a31c676f64b524
cristinaocanamanzano <cristinaocana1@gmail.com>
2018-12-15 19:56:52 +0100

    Refactor print_selected_notes by delegating to private function which calculates note index in array

--- lib/note_printer.rb
+++ lib/note_printer.rb
@@ -4,7 +4,13 @@ class Note_printer
   end
 
   def print_selected_note(notes, selected_note)
-    puts notes[selected_note - 1].title
-    puts notes[selected_note - 1].body
+    puts notes[note_index(selected_note)].title
+    puts notes[note_index(selected_note)].body
+  end
+
+  private
+
+  def note_index(selected_note)
+    selected_note - 1
   end
 end
