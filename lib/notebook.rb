022768f1263b0b8565ae88241d77fb8121cc35f0
cristinaocanamanzano <cristinaocana1@gmail.com>
2018-12-15 19:14:35 +0100

    Refactor notebook class by injecting note class and fixed unit tests

--- lib/notebook.rb
+++ lib/notebook.rb
@@ -1,21 +1,22 @@
 class Notebook
-  attr_reader :saved_notes
+  attr_reader :saved_notes, :note_class
 
-  def initialize
+  def initialize(note_class = Note)
+    @note_class = note_class
     @saved_notes = []
   end
 
   def add_note(title, body)
-    saved_notes << { title: title, body: body }
+    saved_notes << note_class.new(title, body)
     'Note saved!'
   end
 
   def see_title_list
-    saved_notes.each { |note| puts note[:title] }
+    saved_notes.each { |note| puts note.title }
   end
 
   def select_note(note)
-    puts saved_notes[note][:title]
-    puts saved_notes[note][:body]
+    puts saved_notes[note].title
+    puts saved_notes[note].body
   end
 end
--- spec/notebook_spec.rb
+++ spec/notebook_spec.rb
@@ -1,32 +1,46 @@
 require 'notebook'
 
 describe Notebook do
-  subject(:notebook) { described_class.new }
+  subject(:notebook) { described_class.new(note_class) }
+  let(:note_class) { double :note_class }
+  let(:first_note) { double :first_note, title: 'My first note', body: 'This is my first note' }
+  let(:second_note) { double :second_note, title: 'My second note', body: 'This is my second note' }
 
-  describe '#add_note' do
-    it 'adds a note to the saved notes property array' do
-      notebook.add_note('My first note', 'This is my first note')
-      expect(notebook.saved_notes).to eq [{ title: 'My first note', body: 'This is my first note' }]
-    end
+  context 'when adding one note' do
+    describe '#add_note' do
+      before do
+        allow(note_class).to receive(:new).with('My first note', 'This is my first note').and_return(first_note)
+      end
 
-    it 'confirms that the note have been saved' do
-      expect(notebook.add_note('My first note', 'This is my first note')).to eq 'Note saved!'
+      it 'adds a note to the saved notes property array' do
+        notebook.add_note('My first note', 'This is my first note')
+        expect(notebook.saved_notes).to eq [first_note]
+      end
+
+      it 'confirms that the note have been saved' do
+        expect(notebook.add_note('My first note', 'This is my first note')).to eq 'Note saved!'
+      end
     end
   end
 
-  describe '#see_title_list' do
-    it 'prints a list of note titles to the screen' do
+  context 'after adding two notes' do
+    before do
+      allow(note_class).to receive(:new).with('My first note', 'This is my first note').and_return(first_note)
+      allow(note_class).to receive(:new).with('My second note', 'This is my second note').and_return(second_note)
       notebook.add_note('My first note', 'This is my first note')
       notebook.add_note('My second note', 'This is my second note')
-      expect { notebook.see_title_list }.to output("My first note\nMy second note\n").to_stdout
     end
-  end
 
-  describe '#select_note' do
-    it 'shows title and body of note selected by user' do
-      notebook.add_note('My first note', 'This is my first note')
-      notebook.add_note('My second note', 'This is my second note')
-      expect { notebook.select_note(1) }.to output("My second note\nThis is my second note\n").to_stdout
+    describe '#see_title_list' do
+      it 'prints a list of note titles to the screen' do
+        expect { notebook.see_title_list }.to output("My first note\nMy second note\n").to_stdout
+      end
+    end
+
+    describe '#select_note' do
+      it 'shows title and body of note selected by user' do
+        expect { notebook.select_note(1) }.to output("My second note\nThis is my second note\n").to_stdout
+      end
     end
   end
 end
