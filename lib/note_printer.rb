5332a8bd61ccca024442127c2abb0217d31136e3
cristinaocanamanzano <cristinaocana1@gmail.com>
2018-12-16 21:19:17 +0000

    Refactor print selected note function by delegating to private function

--- lib/note_printer.rb
+++ lib/note_printer.rb
@@ -5,7 +5,7 @@ class Note_printer
   end
 
   def print_selected_note(notes, selected_note)
-    raise 'That note does not exit, please select another number' if selected_note > notes.size
+    raise 'That note does not exit, please select another number' unless note_exists?(notes, selected_note)
     puts notes[note_index(selected_note)].title
     puts notes[note_index(selected_note)].body
   end
@@ -18,5 +18,9 @@ class Note_printer
 
   def notebook_emtpy?(notes)
     notes.empty?
+  end
+
+  def note_exists?(notes, selected_note)
+    selected_note <= notes.size
   end
 end
