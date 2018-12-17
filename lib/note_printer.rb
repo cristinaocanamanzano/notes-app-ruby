46e9c58cc36f6c4fb082f2421bb60fc5526b4886
cristinaocanamanzano <cristinaocana1@gmail.com>
2018-12-17 18:19:25 +0000

    Print selected note uses method show_title instead of title property

--- lib/note_printer.rb
+++ lib/note_printer.rb
@@ -6,7 +6,7 @@ class Note_printer
 
   def print_selected_note(notes, selected_note)
     raise 'That note does not exit, please select another number' unless note_exists?(notes, selected_note)
-    puts notes[note_index(selected_note)].title
+    puts notes[note_index(selected_note)].show_title
     puts notes[note_index(selected_note)].body
   end
 
